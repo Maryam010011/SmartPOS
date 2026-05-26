30fd1e295cce3feb3f1188beb532206b53b4d282
Maryam010011 <maryamjahangir010011@gmail.com>
2026-05-27 01:04:52 +0500

    Add Newtonsoft & Stripe DLLs; update assembly info
    Add Newtonsoft.Json.dll and Stripe.net.dll to bin/Debug/net10.0 and include a new compressed artifact in obj/Debug/net10.0/compressed. Update AssemblyInformationalVersion in obj/Debug/net10.0/SmartPOS.AssemblyInfo.cs (build metadata changed) and refresh the AssemblyInfoInputs.cache. These are build output changes from a Debug build.

--- obj/Debug/net10.0/SmartPOS.AssemblyInfo.cs
+++ obj/Debug/net10.0/SmartPOS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartPOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48012d38debaf5ee58c85ca64efcacf2301c2fb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa2c0be7fc2a5fbbcb8f690bbefd6f34f8fdb4d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartPOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartPOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
